77b23bc061e53f876c4a248a555bf7ec0497385b
zhangwei51 <zhangwei51@cn.panasonic.com>
2024-11-13 10:49:59 +0800

    测试

--- swagger-test/src/main/java/com/test/controller/Test2Controller.java
+++ swagger-test/src/main/java/com/test/controller/Test2Controller.java
@@ -32,7 +32,7 @@ public class Test2Controller {
 	})
 	@PostMapping("testMap")
 	public Map<String, Object> testMap(@RequestBody Map<String, Object> param){
-		//测试远程分支1
+		//测试本地分支
 		return param;
 	}
 	
